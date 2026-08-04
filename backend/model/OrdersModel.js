e5f7f25ae52501483a4bf7f5259acc68a0f61b6f
Satyam Rajbhar <rajbharsatyam661@gmail.com>
2026-08-04 06:01:24 +0530

    Updated Project

--- backend/model/OrdersModel.js
+++ backend/model/OrdersModel.js
@@ -2,6 +2,7 @@ const { model } = require("mongoose");
 
 const { OrdersSchema } = require("../schemas/OrdersSchema");
 
+
 const OrdersModel = new model("order", OrdersSchema);
 
 module.exports = { OrdersModel };
--- backend/schemas/OrdersSchema.js
+++ backend/schemas/OrdersSchema.js
@@ -5,6 +5,7 @@ const OrdersSchema = new Schema({
   qty: Number,
   price: Number,
   mode: String,
+  
 });
 
 module.exports = { OrdersSchema };
